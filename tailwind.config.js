af7f80e6dcb41270381eea07347dc0267ec450d1
Alvax502-debug <alvarorodriguezmunoz51@gmail.com>
2023-08-31 01:23:59 -0600

    Carga de archivos para trigesimo noveno cloud build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './storage/framework/views/*.php',
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
